df33bd8b473341871b5114ef6b1f00974607e5af
MSRbuddy <meghanadasari222@gmail.com>
2023-04-20 15:12:52 +0530

    Added : Stack program UC-1!

---
+++ Stack-UC1/Stack-UC1/Stack-UC1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Stack_UC1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StackUsingLl myStack = new StackUsingLl();
+            myStack.Push(70);
+            myStack.Push(30);
+            myStack.Push(56);
+
+            myStack.Display();
+            Console.WriteLine("Top element in Stack is {0} ", myStack.Peek());
+        }
+        public class StackUsingLl
+        {
+            private class Node
+            {
+                public int data;
+                public Node link;
+            }
+            Node top;
+            public StackUsingLl()
+            {
+                this.top = null;
+            }
+            public void Push(int d)
+            {
+                Node temp = new Node();
+                if (temp == null)
+                {
+                    Console.WriteLine("Stack Overflow");
+                    return;
+                }
+                temp.data = d;
+                temp.link = top;
+                top = temp;
+            }
+            public bool isEmpty()
+            {
+                return top == null;
+            }
+            public int Peek()
+            {
+                if (!isEmpty())
+                {
+                    return top.data;
+                }
+                else
+                {
+                    Console.WriteLine("Stack is empty!");
+                    return -1;
+                }
+            }
+            public void Display()
+            {
+                if (top == null)
+                {
+                    Console.WriteLine("Stack Underflow");
+                    return;
+                }
+                else
+                {
+                    Node temp = top;
+                    while (temp != null)
+                    {
+                        Console.WriteLine(temp.data);
+                        temp = temp.link;
+                    }
+                }
+            }
+        }
+    }
+}
+
+
